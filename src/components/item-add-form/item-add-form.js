48bff2941ce873300bbbf14245001de4138325c5
zhazaira <zmahanzazira@gmail.com>
2024-04-05 10:36:02 +0500

    react commit

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import './item-add-form.css';
 
-export default class ItemAddForm extends  Component {
+export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault(); // Fixed typo
+        this.props.OnItemAdded(this.state.label);
+        this.setState({ label: '' }); // Clear the input after submitting
+    };
 
     render() {
         return (
-            <div className={ "item-add-form" }>
-                <button
-                    className={ "btn btn-outline-secondary"}
-                    onClick={() => this.props.OnItemAdded('Hello World')}>
+            <form className={"item-add-form d-flex"} onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                    value={this.state.label} // Controlled input value
+                />
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
